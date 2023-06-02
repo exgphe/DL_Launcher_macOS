9cf915dbfa4b7c6f241540de75bf7c4296c472c6
exgphe <exgphe@gmail.com>
2023-06-02 14:49:21 +0200

    Initial Commit

---
+++ DL Launcher/DL_LauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DL_LauncherApp.swift
+//  DL Launcher
+//
+//  Created by Xiaolin Wang on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DL_LauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
